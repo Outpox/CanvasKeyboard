4f92c45d61d578b502021b2c1b4c27410d45faef
Outpox <guillaumeoutpox@gmail.com>
2015-11-03 11:35:05 +0100

    Cleared code

--- index.js
+++ index.js
@@ -48,17 +48,3 @@ fontTypeInput.addEventListener("change", function () {
     fontType.innerHTML = fontTypeInput.value;
     kb.keyFontType = fontTypeInput.value;
 });
-
-//ctx.beginPath();
-//ctx.moveTo(550, 10);
-//
-//ctx.lineTo(620, 10); //620
-//ctx.lineTo(620, 95);
-//ctx.lineTo(560, 95);
-//ctx.lineTo(560, 50);
-//ctx.lineTo(550, 50);
-//ctx.lineTo(550, 10);
-//
-//ctx.strokeStyle = "red";
-//ctx.lineWidth = 1;
-//ctx.stroke();
